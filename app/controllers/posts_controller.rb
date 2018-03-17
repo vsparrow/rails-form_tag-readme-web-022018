ee215d2b46fc54761c60147630ccfde976e9f4d4
vsparrow <khan@vsparrow.com>
2018-03-17 12:18:31 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
-  
+
+  def create
+    # @post= Post.create(params[:post]) #we dont need instance since not passing forward data
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
+  def new
+  end
+
 end
